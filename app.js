2e30f67ade185c5e7e4e8839aa382605d5115e28
dine1817 <156121040+dine1817@users.noreply.github.com>
2024-01-17 21:42:51 +0530

    responsive done

--- app.js
+++ app.js
@@ -10,40 +10,29 @@ let callMe = document.getElementById("calMethod");
 
 
 function clearScreen(){
-    if(onBtn.value == "TURN ON" || onBtn.value == "OFF"){
-        msg.innerText = "Hello Buddy, just \"TURN ON\" the calculator.";
+    if(onBtn.value == "OFF"){
+        msg.innerText = "Press the \"OFF\" button to start the calculator.";
+    }else if(onBtn.value == "TURN ON" || onBtn.value == "OFF"){
+        msg.innerText = "Press the \"TURN ON\" button to start the calculator.";
     }else{
         resultInp.value = "";
     }
 }
 function display(value){
-    if(onBtn.value == "TURN ON" || onBtn.value == "OFF"){
-        msg.innerText = "Hello Buddy, just \"TURN ON\" the calculator.";
+    if(onBtn.value == "OFF"){
+        msg.innerText = "Press the \"OFF\" button to start the calculator.";
+    }else if(onBtn.value == "TURN ON"){
+        msg.innerText = "Press the \"TURN ON\" button to start the calculator.";
     }else{
         resultInp.value += value;
     }
 }
-//calMethod
-// callMe.addEventListener("click",function(event){
-//     event.preventDefault();
-//     if(onBtn.value == "TURN ON" || onBtn.value == "OFF"){
-//         msg.innerText = "Hello Buddy, just \"TURN ON\" the calculator.";
-//     }else{
-//         let input = resultInp.value;
-//         let ans = eval(input);
-//         resultInp.value = ans;
-//         nameRes.value = ans;
-//         console.log(nameRes.value);
-//         if(resultInp.value == "undefined"){
-//             resultInp.value = "";
-//         }
-//     }
-// });
-function calculate(){
-    
-    if(onBtn.value == "TURN ON" || onBtn.value == "OFF"){
-        msg.innerText = "Hello Buddy, just \"TURN ON\" the calculator.";
-    }else{
+function calculate(){  
+    if(onBtn.value == "OFF"){
+        msg.innerText = "Press the \"OFF\" button to start the calculator.";
+    }else if(onBtn.value == "TURN ON"){
+        msg.innerText = "Press the \"TURN ON\" button to start the calculator.";
+    }else if(onBtn.value != "OFF" && onBtn.value != "TURN ON"){
         let input = resultInp.value;
         let ans = eval(input);
         resultInp.value = ans;
@@ -56,7 +45,7 @@ function calculate(){
 }
 function clearLast(){
     if(onBtn.value == "TURN ON" || onBtn.value == "OFF"){
-        msg.innerText = "Hello Buddy, just \"TURN ON\" the calculator.";
+        msg.innerText = "Press the \"TURN ON\" button to start the calculator.";
     }else{
         let ans = resultInp.value;
         let last = ans.length-1;
@@ -69,39 +58,42 @@ function onOffFunction(){
     if(ans == "TURN ON"){
         onBtn.innerText = "ON";
         onBtn.value = onBtn.innerText = "ON";
-        onBtn.style.backgroundColor = "#839299";
+        onBtn.style.backgroundColor = "#5D5AFF";
         msg.innerText = "";
     }else if(ans == "ON"){
         onBtn.innerText = "OFF";
         onBtn.value = onBtn.innerText = "OFF";
-        onBtn.style.backgroundColor = "rgb(171, 180, 185)";
+        onBtn.style.backgroundColor = "#5D5AFF";
         resultInp.value = "";
     }else if(ans == "OFF"){
         onBtn.innerText = "ON";
         onBtn.value = onBtn.innerText = "ON";
-        onBtn.style.backgroundColor = "#839299";
+        onBtn.style.backgroundColor = "#5D5AFF";
         msg.innerText = "";
     }
 }
 let darkResult = false;
 function darkMode(){
     if(darkResult == false){
-        bodyArea.style.backgroundColor = "black";
-        bodyArea.style.color = "#fff";
-        sectionArea.style.backgroundColor = "black";
-        sectionArea.style.border = "2px solid #fff";
-        resultArea.style.backgroundColor = "#E6F0F9";
-        msg.style.color = "#fff";
+        bodyArea.style.backgroundColor = "rgba(0, 0, 0, 0.495)";
+        bodyArea.style.color = "#2F3547";
+        sectionArea.style.backgroundColor = "rgba(0, 0, 0, 0.495)";
+        sectionArea.style.border = "4px solid #2F3547";
+        resultArea.style.backgroundColor = "#2F3547";
+        msg.style.color = "#E5EFF8";
         darkResult = true;
+        document.getElementById("lightDiv").style.display = "block";
+        document.getElementById("darkDiv").style.display = "none";
+
     }else if(darkResult == true){
-        bodyArea.style.backgroundColor = "rgb(83, 81, 81)";
+        bodyArea.style.backgroundColor = "#E760E1";
         bodyArea.style.color = "black";
-        sectionArea.style.backgroundColor = "rgb(229,239,248)";
-        sectionArea.style.border = "none";
-        resultArea.style.backgroundColor = "#839299";
-        msg.style.color = "black";
+        sectionArea.style.backgroundColor = "#E760E1";
+        sectionArea.style.border = "5px solid #5D5AFF";
+        resultArea.style.backgroundColor = "#5D5AFF";
+        msg.style.color = "whitesmoke";
         darkResult = false;
-    }
-    
-    
+        document.getElementById("lightDiv").style.display = "none";
+        document.getElementById("darkDiv").style.display = "block";
+    }   
 }
